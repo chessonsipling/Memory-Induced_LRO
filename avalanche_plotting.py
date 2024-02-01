b93663343e78294dc5a09f4c3c19291924c5d7c2
Chesson Sipling <64238068+chessonsipling@users.noreply.github.com>
2024-02-01 14:17:17 -0800

    Update avalanche_plotting.py

--- avalanche_plotting.py
+++ avalanche_plotting.py
@@ -36,14 +36,15 @@ def single_avalanche_plot(param_string, characterizing_text, fit):
     if fit == 'sf':
         try:
             #Performs parameter fit on avalanche distribution
-            parameters_scale_free, covariance_scale_free = curve_fit(scale_free, sizes[3:25], percentages[3:25], p0=[-3, 5000], bounds=([-np.inf, 0], [0, np.inf])) #[0:75] for [32x32], g=0.001; [1:50] for [32x32], g=0.01; [5:200] for [64x64], g=0.01; [5:100] for [64x64, g=0.001, 0.003]; [20:200] for [96x96], g=0.01
+            parameters_scale_free, covariance_scale_free = curve_fit(scale_free, sizes[10:75], percentages[10:75], p0=[-1.7, 1], bounds=([-np.inf, 0], [0, np.inf]))
             stddev_error_scale_free = np.sqrt(np.diag(covariance_scale_free))
             #Generates data arrays from the fitted parameters
             x_fit = np.arange(1, max(avalanche_sizes) + 1, 1)
-            gaussian_fit = []
+            scale_free_fit = []
             for i in range(len(x_fit)):
                 scale_free_fit.append(scale_free(x_fit[i], parameters_scale_free[0], parameters_scale_free[1]))
-            plt.plot(x_fit, scale_free_fit, color='red', linestyle='dashed', label='Scale Free Fit: a = %1.2f +/- %1.2f' % (parameters_scale_free[0], stddev_error_scale_free[0])) #plots this fitted data alongside the raw data of (sizes, counts)
+            plt.plot(x_fit, scale_free_fit, color='red', linestyle='dashed')#, label='Scale Free Fit: a = %1.2f +/- %1.2f' % (parameters_scale_free[0], stddev_error_scale_free[0])) #plots this fitted data alongside the raw data of (sizes, counts)
+            plt.text(45, 1e-3, '~ $s^{%1.2f}$' % parameters_scale_free[0], color='red')
         except:
             print('Fitting failed, try different fitting parameter guesses')
 
@@ -52,10 +53,10 @@ def single_avalanche_plot(param_string, characterizing_text, fit):
             parameters_gaussian, covariance_gaussian = curve_fit(gaussian, sizes[1:-10], percentages[1:-10], p0=[sz[0]/10, sz[0]/20, 15000], bounds=([0, 0, 0], [np.inf, np.inf, np.inf]))
             stddev_error_gaussian = np.sqrt(np.diag(covariance_gaussian))
             x_fit = np.arange(1, max(avalanche_sizes) + 1, 1)
-            scale_free_fit = []
+            gaussian_fit = []
             for i in range:
                 gaussian_fit.append(gaussian(x_fit[i], parameters_gaussian[0], parameters_gaussian[1], parameters_gaussian[2]))
-            plt.plot(x_fit, gaussian_fit, color='red', linestyle='dashed', label='Gaussian Fit: Mu =  %1.2f +/- %1.2f, Sigma = %1.2f +/- %1.2f' % (parameters_gaussian[0], stddev_error_gaussian[0], parameters_gaussian[1], stddev_error_gaussian[1]))
+            plt.plot(x_fit, gaussian_fit, color='red', linestyle='dashed')#, label='Gaussian Fit: Mu =  %1.2f +/- %1.2f, Sigma = %1.2f +/- %1.2f' % (parameters_gaussian[0], stddev_error_gaussian[0], parameters_gaussian[1], stddev_error_gaussian[1]))
         except:
             print('Fitting failed, try different fitting parameter guesses')
 
@@ -92,8 +93,8 @@ def avalanche_plotting(general_params, distribution_type, time_window, flip_axis
 def plot_all_avalanches(size_list, common_general_params, common_misc_params, finite_size):
 
     #Some formatting specfications (can be adjusted/added to)
-    ###plt.rcParams.update({'font.size': 22, 'legend.loc': 'lower left'})
-    plt.rcParams.update({'font.size': 22, 'legend.loc': 'upper right'})
+    plt.rcParams.update({'font.size': 22, 'legend.loc': 'lower left'})
+    ###plt.rcParams.update({'font.size': 22, 'legend.loc': 'upper right'})
     plt.subplots_adjust(bottom=0.2, left=0.2)
 
     #Initializes list of arrays, providing comprehensive data on all avalanche distributions
@@ -118,12 +119,19 @@ def plot_all_avalanches(size_list, common_general_params, common_misc_params, fi
         list_of_arraysizes.append(sz)
 
         #Extracts the x and y data from .txt files, performing fit if desired
-        sizes, percentages = single_avalanche_plot(get_param_string(sz, common_general_params[0], common_general_params[1], common_general_params[2], common_general_params[3], common_general_params[4], common_general_params[5], common_general_params[7]), characterizing_text, fit)
+        if fit == 'sf':
+            if sz == size_list[-1]:
+                sizes, percentages = single_avalanche_plot(get_param_string(sz, common_general_params[0], common_general_params[1], common_general_params[2], common_general_params[3], common_general_params[4], common_general_params[5], common_general_params[7]), characterizing_text, fit)
+            else:
+                sizes, percentages = single_avalanche_plot(get_param_string(sz, common_general_params[0], common_general_params[1], common_general_params[2], common_general_params[3], common_general_params[4], common_general_params[5], common_general_params[7]), characterizing_text, False)
+        else:
+            sizes, percentages = single_avalanche_plot(get_param_string(sz, common_general_params[0], common_general_params[1], common_general_params[2], common_general_params[3], common_general_params[4], common_general_params[5], common_general_params[7]), characterizing_text, fit)
         list_of_sizes.append(sizes)
         list_of_percentages.append(percentages)
 
         plt.scatter(sizes, percentages, label=get_sz_string(sz))
 
+    plt.ylim(1e-6, 1)
     plt.xlabel('Avalanche Size (s)')
     plt.ylabel('P(s)')
     plt.legend()
@@ -134,7 +142,7 @@ def plot_all_avalanches(size_list, common_general_params, common_misc_params, fi
 
     #Perform scale invariance analysis
     if finite_size[0] == True:
-        plt.rcParams.update({'legend.loc': 'upper left'})
+        ###plt.rcParams.update({'legend.loc': 'upper left'})
         for i in range(len(size_list)):
             finite_size_x = [element/((list_of_arraysizes[i][0]*list_of_arraysizes[i][1])**finite_size[2]) for element in list_of_sizes[i]]
             finite_size_y = [a*b for a,b in zip([element**finite_size[1] for element in list_of_sizes[i]],list_of_percentages[i])]
